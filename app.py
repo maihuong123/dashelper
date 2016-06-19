43f3d1162f6e43e7f3918d443733167a8bbf362b
Mai Huong <maihuong.nt1993@gmail.com>
2016-06-19 10:25:28 +0700

    code

---
+++ app.py
@@ -0,0 +1,117 @@
+from flask import Flask, render_template,request,redirect,url_for
+#---------------------------------- connect database lowcarb
+import mongoengine
+from mongoengine import Document, StringField
+host = "ds025973.mlab.com"
+port = 25973
+db_name = "data_kcal"
+user_name = "huong"
+password = "123456"
+mongoengine.connect(db_name, host=host, port=port, username=user_name, password=password)
+
+class Carb_recipes(Document):
+    title = StringField()
+    img = StringField()
+    link = StringField()
+    summary = StringField()
+#-------------------------
+#---------------------------------- connect database shop
+import mongoengine
+from mongoengine import Document, StringField
+host = "ds025973.mlab.com"
+port = 25973
+db_name = "data_kcal"
+user_name = "huong"
+password = "123456"
+mongoengine.connect(db_name, host=host, port=port, username=user_name, password=password)
+
+class Shop_das(Document):
+    title = StringField()
+    link = StringField()
+    image = StringField()
+#-------------------------
+app = Flask(__name__,static_url_path='')
+#----------------------- search
+import pymongo
+db_uri = "mongodb://huong:123456@ds025973.mlab.com:25973/data_kcal"
+
+db = pymongo.MongoClient(db_uri).get_default_database()
+kcal_collection = db['kcal']
+
+
+
+
+@app.route('/')
+def index():
+    # n = str(input("Enter your food: ")).upper()
+    # def collect_choice(x):
+    #     data_find = kcal_collection.find({'NAME': x})
+    #     for i in data_find:
+    #         print(i['SHORT_DESCRIPT'])
+    #         print("Traffic_light: ", i['TRAFFIC_LIGHT'])
+    #         print("carb: ", i['CARB'])
+    #         print("kcal: ", i['KCAL'])
+    #         print('-------------------')
+    # collect_choice(n)
+    return render_template("index.html")
+
+@app.route('/index')
+def index_return():
+    return render_template("index.html")
+# -------------------------------------------------
+@app.route('/lowcarb')
+def lowcarb():
+    return render_template("lowcarb.html",data = Carb_recipes.objects)
+
+@app.route('/shop')
+def shop():
+    return render_template("shop.html",data_shop = Shop_das.objects)
+
+@app.route('/orders')
+def orders():
+    return render_template("orders.html")
+#--------------------------------------------------BMI
+
+
+@app.route('/BMI_request<bmr>,<BMI>,<calo>')
+def BMI_request(bmr,BMI,calo):
+    return render_template('BMI_request.html',bmr=bmr,BMI=BMI,calo=calo)
+
+@app.route('/BMI',methods=['GET','POST'])
+def bmi():
+    if request.method == 'POST':
+        if request.form['gender'] == "Male":
+            bmr = 66.47 + (13.75  * int(request.form['weight'])) + (5.0 * int(request.form['height'])) - (6.75 * int(request.form['age']))
+            BMI = int(request.form['weight']) / (int(request.form['height']) / 100) ** 2
+        elif request.form['gender'] == "Female":
+            bmr = 665.09 + (9.56 * int(request.form['weight'])) + (1.84 * int(request.form['height'])) - (4.67 * int(request.form['age']))
+            BMI = int(request.form['weight']) / (int(request.form['height']) / 100) ** 2
+
+        if request.form['Level of activity'] == "1":
+            calo = bmr * 1.2
+        elif request.form['Level of activity'] == "2":
+            calo = bmr * 1.35
+        elif request.form['Level of activity'] == "3":
+            calo = bmr * 1.55
+        elif request.form['Level of activity'] == "4":
+            calo = bmr * 1.75
+        elif request.form['Level of activity'] == "5":
+            calo = bmr * 1.95
+
+        return redirect(url_for('BMI_request', bmr=bmr, BMI=BMI, calo = calo))
+        return render_template("BMI.html")
+#--------------------------------------------------------------------
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/login',methods=['GET','POST'])#vut thuoc tinh vao day
+def login():
+    if request.method == 'POST':
+        return redirect('/')
+    return render_template("login.html")
+
+if __name__ == '__main__':
+    app.run()
